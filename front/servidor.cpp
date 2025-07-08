195c5a4080c6e17a4e3029ed6c0b505f5ee79f6a
Pamela Villar <astropamela20@gmail.com>
2025-07-08 11:21:04 -0500

    algo

--- front/servidor.cpp
+++ front/servidor.cpp
@@ -124,21 +124,11 @@ int main() {
       std::vector<std::pair<int, std::vector<std::string>>> registros =
           sql.getRegistros(bites);
 
-/*lo nuevo que agregue */
-    //  std::vector<std::pair<UbicacionFisica, UbicacionFisica>> ubicaciones;
-    //  std::vector<std::pair<int, std::vector<std::string>>> registros = sql.getRegistrosConUbicacion(ubicaciones);
-/*aqui se acaba esta parte */
+
 
       nlohmann::json data_json = nlohmann::json::array();
       std::cout << "registros size: " << registros.size() << "\n";
-/* esto estariamos quintando
-      for (const auto &[id, fila] : registros) {
-        nlohmann::json fila_json;
-        fila_json["id"] = id;
-        fila_json["valores"] = fila;
-        data_json.push_back(fila_json);
-      }
-*/
+
 
         for (size_t i = 0; i < registros.size(); ++i) {
           const auto &[id, fila] = registros[i];
@@ -170,7 +160,7 @@ int main() {
 
       nlohmann::json resultado;
       resultado["status"] = "success";
-      resultado["data"] = data_json; // Esto asumo que ya lo construiste
+      resultado["data"] = data_json; //
       resultado["headers"] = headers_json;
 
       std::cout << resultado.dump(4) << std::endl; // ESTA LINEA MAS AGREGUEEE
@@ -190,32 +180,7 @@ int main() {
     }
   });
 
-  // svr.Post("/buscar", [](const httplib::Request& req, httplib::Response& res)
-  // {
-  //     res.set_header("Access-Control-Allow-Origin", "*");
-  //     res.set_header("Access-Control-Allow-Methods", "POST, OPTIONS");
-  //     res.set_header("Access-Control-Allow-Headers", "Content-Type");
 
-  //     if (!dbmanager) {
-  //         res.status = 500;
-  //         res.set_content("Base de datos no configurada", "text/plain");
-  //         return;
-  //     }
-
-  //     try {
-  //         json payload = json::parse(req.body);
-  //         //json resultados = procesarPeticionCompleta(payload, dbmanager);
-  //         json resultados = procesarPeticionCompleta(payload, dbmanager);
-  //         cout<< resultados<<"\n";
-  //         res.set_content(resultados.dump(), "application/json");
-  //     } catch (const exception& e) {
-  //         cerr << "Error al procesar búsqueda: " << e.what() << endl;
-  //         res.status = 400;
-  //         res.set_content("Error al procesar búsqueda", "text/plain");
-  //     }
-  // });
-
-  // Rutas OPTIONS para CORS
   svr.Options("/configurar",
               [](const httplib::Request &req, httplib::Response &res) {
                 res.set_header("Access-Control-Allow-Origin", "*");
